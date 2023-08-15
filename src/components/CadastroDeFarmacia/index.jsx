0725e14f5d066968370a7ffd9835c94db400e8b7
Andre Silveira <andrehahia@gmail.com>
2023-08-14 23:03:38 -0300

    darc-styled-login

--- src/components/CadastroDeFarmacia/index.jsx
+++ src/components/CadastroDeFarmacia/index.jsx
@@ -26,23 +26,23 @@ function CadastroDeFarmacia (){
         <p> Cadastro de Farmácia </p>
 
         <div>
-        <TextField label="Razão Social" required = {true} variant="outlined" value={razaoSocial} onChange={setRazaoSocial} />
-        <TextField label="CNPJ" required = {true} variant="outlined" value={cnpj} />
-        <TextField label="Nome Fantasia" required = {true} variant="outlined" value={nomeFantasia} />
-        <TextField label="E-Mail" required = {true} variant="outlined" value={email} />
-        <TextField label="Telefone" required = {true} variant="outlined" value={telefone} />
-        <TextField label="Celular" required = {true} variant="outlined" value={celular} />
+        <TextField label="Razão Social" required = {true} variant="outlined" value={razaoSocial} onChange={(e) => setRazaoSocial(e.target.value)} />
+        <TextField label="CNPJ" required = {true} variant="outlined" value={cnpj} onChange={(e) => setCnpj(e.target.value)} />
+        <TextField label="Nome Fantasia" required = {true} variant="outlined" value={nomeFantasia} onChange={(e) => setNomeFantasia(e.target.value)} />
+        <TextField label="E-Mail" required = {true} variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <TextField label="Telefone" required = {true} variant="outlined" value={telefone} onChange={(e) => setTelefone(e.target.value)} />
+        <TextField label="Celular" required = {true} variant="outlined" value={celular} onChange={(e) => setCelular(e.target.value)} />
         </div>
         <div>
-        <TextField label="CEP" required = {true} variant="outlined" value={cep} />
-        <TextField label="Logradouro" required = {true} variant="outlined" value={logradouro} />
-        <TextField label="Numero" required = {true} variant="outlined" value={numero} />
-        <TextField label="Bairro" required = {true} variant="outlined" value={bairro} />
-        <TextField label="Cidade" required = {true} variant="outlined" value={cidade} />
-        <TextField label="Estado" required = {true} variant="outlined" value={estado} />
-        <TextField label="Complemento" required = {true} variant="outlined" value={complemento} />
-        <TextField label="Latitude" required = {true} variant="outlined" value={latitude} />
-        <TextField label="Longitude" required = {true} variant="outlined" value={longitude} />
+        <TextField label="CEP" required = {true} variant="outlined" value={cep} onChange={(e) => setCep(e.target.value)} />
+        <TextField label="Logradouro" required = {true} variant="outlined" value={logradouro} onChange={(e) => setLogradouro(e.target.value)} />
+        <TextField label="Numero" required = {true} variant="outlined" value={numero} onChange={(e) => setNumero(e.target.value)} />
+        <TextField label="Bairro" required = {true} variant="outlined" value={bairro} onChange={(e) => setBairro(e.target.value)} />
+        <TextField label="Cidade" required = {true} variant="outlined" value={cidade} onChange={(e) => setCidade(e.target.value)} />
+        <TextField label="Estado" required = {true} variant="outlined" value={estado} onChange={(e) => setEstado(e.target.value)} />
+        <TextField label="Complemento" required = {true} variant="outlined" value={complemento} onChange={(e) => setComplemento(e.target.value)} />
+        <TextField label="Latitude" required = {true} variant="outlined" value={latitude} onChange={(e) => setLatitude(e.target.value)} />
+        <TextField label="Longitude" required = {true} variant="outlined" value={longitude} onChange={(e) => setLongitude(e.target.value)} />
         </div>
         <div>
         <Button variant="contained"> LIMPAR </Button>
