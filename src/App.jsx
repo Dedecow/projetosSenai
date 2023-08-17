ead782b646823a3e1ea57a7974d4d90919fdd1c6
Andre Silveira <andrehahia@gmail.com>
2023-08-17 20:55:07 -0300

    renderizando-maps

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
 // import { Header } from './components/Header'
 // import { CadastroFarmaciaPage } from './pages/cadastroDeFarmacias'
-import { CadastroDeMedicamentosPage } from './pages/cadastroDeMedicamentos'
+// import { CadastroDeMedicamentosPage } from './pages/cadastroDeMedicamentos'
 // import { LoginPage } from './pages/login'
-// import { MapaDeFarmaciasPage } from './pages/mapaDeFarmacias'
+import { MapaDeFarmaciasPage } from './pages/mapaDeFarmacias'
 // import { MedicamentosPage } from './pages/medicamentos'
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
       {/* <LoginPage/> */}
       {/*<p> PRIMEIRO COMMIT - PROJETO DEVINHOUSE - MODULO 1 </p> */}
       {/* <CadastroFarmaciaPage/> */}
-      <CadastroDeMedicamentosPage/>
-      {/* <MapaDeFarmaciasPage/> */}
+      {/* <CadastroDeMedicamentosPage/> */}
+      <MapaDeFarmaciasPage/>
       {/* <MedicamentosPage/> */}
     </>
   )
