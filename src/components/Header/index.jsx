42caf3dc4c141fe8755b24f24dded20156b2b7c7
Andre Silveira <andrehahia@gmail.com>
2023-08-17 10:39:30 -0300

    rotas-definidas

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,17 +5,29 @@ function Header() {
   const pages = [
     {
       route: "/cadastroMedicamentos",
-      description: "Cadastro de Medicamentos"
+      description: "Página de Cadastro de Medicamentos"
     },
     {
       route: "/login",
       description: "Página de Login"
     }
+    {
+      route: "/cadastroDeFarmacias",
+      description: "Página de cadastro das farmácias"
+    }
+    {
+      route: "/medicamentos",
+      description: "Página de Relatório dos ítens cadastrados"
+    }
+    {
+      route: "/mapaDeFarmacias",
+      description: "Página de localização das fármácias cadastradas"
+    }
   ];
 
   return (
     <form className="container">
-      <h3>Cabeçalho</h3>
+      <h3 className="titulo">Cabeçalho</h3>
           {pages.map(({ route, description }) => (
             <Link key={route} to={route}>
               {description}
