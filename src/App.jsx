0e161465c582134191b07e1903ba050ce9c0cd5f
Andre Silveira <andrehahia@gmail.com>
2023-08-18 09:45:46 -0300

    fix-conflito

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import './App.css'
-// import { LoginPage } from './pages/login'
-import { CadastroFarmaciaPage } from './pages/cadastroDeFarmacias'
-// import { CadastroDeMedicamentosPage } from './pages/cadastroDeMedicamentos'
-
-
-// import { HeaderComponent } from './components/Header'
-import { MapaDeFarmaciasPage } from './pages/mapaDeFarmacias'
-// import { MedicamentosPage } from './pages/medicamentos'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { LoginPage } from "./pages/login";
+import { CadastroFarmaciaPage } from "./pages/cadastroDeFarmacias";
+import { CadastroDeMedicamentosPage} from "./pages/cadastroDeMedicamentos";
+import { MapaDeFarmaciasPage } from "./pages/mapaDeFarmacias"
+import { MedicamentosPage } from "./pages/medicamentos";
 
 function App() {
   return (
-    <>
-      {/* <LoginPage/> */}
-      <CadastroFarmaciaPage/>
-      {/* <CadastroDeMedicamentosPage/> */}
-
-
-      {/* < HeaderComponent/> */}
-      <MapaDeFarmaciasPage/>
-      {/* <MedicamentosPage/> */}
-    </>
-  )
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={LoginPage} />
+        <Route path="/cadastro-farmacia" component={CadastroFarmaciaPage} />
+        <Route path="/cadastro-medicamento" component={CadastroDeMedicamentosPage} />
+        <Route path="/mapa" component={MapaDeFarmaciasPage} />
+        <Route path="/lista-medicamentos" component={MedicamentosPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
