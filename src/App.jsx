be8d892964bec0a699bb88f8f755e2c91bed6a8b
Andreh Silveira <andrehbahia@gmail.com>
2023-08-20 18:06:13 -0300

    navbar-page

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ function App() {
   return (
     <>
       <NavBar/>
-      <Outlet/>
-      <MedicamentosPage/>
+      {/* <Outlet/> */}
+      {/* <MedicamentosPage/>
       <CadastroFarmaciaPage/>
-      <MapaDeFarmaciasPage/>
+      <MapaDeFarmaciasPage/> */}
       
     </>
   )
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,35 +1,21 @@
 import { Link } from "react-router-dom";
 
 function NavBar() {
-  // const pages = [
-  //   {
-  //     route: "/cadastroMedicamentos",
-  //     description: "Página de Cadastro de Medicamentos"
-  //   },
-  //   {
-  //     route: "/login",
-  //     description: "Página de Login"
-  //   },
-  //   {
-  //     route: "/cadastroDeFarmacias",
-  //     description: "Página de cadastro das farmácias"
-  //   },
-  //   {
-  //     route: "/medicamentos",
-  //     description: "Página de Relatório dos ítens cadastrados"
-  //   },
-  //   {
-  //     route: "/mapaDeFarmacias",
-  //     description: "Página de localização das fármácias cadastradas"
-  //   }
-  // ];
+  
 
   return (
-    <nav >
-      <Link to="/map"></Link>
-      <Link to="/cadastroDeMedicamento"></Link>
-      <Link to="/cadastroDeFarmacias"></Link>
-      <Link to="/medicamentos"></Link>
+    <nav className="container">
+      <ul>
+        <li className="li"><Link to="/map">Mapa de Farmácias</Link></li>
+        <li className="li"><Link to="/cadastroDeMedicamento">Cadastrar Medicamentos</Link></li>
+        <li className="li"><Link to="/cadastroDeFarmacias">Cadastrar Farmácias</Link></li>
+        <li className="li"><Link to="/medicamentos">Listar Medicamentos</Link></li>
+        <li className="li"><Link to="/Login">Tela Inicial</Link></li>
+      </ul>
+      
+      
+      
+      
     </nav>
     
   );
