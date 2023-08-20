5f84bf0d583fef4d6c3f545a549f21fdb4a7b188
Andreh Silveira <andrehbahia@gmail.com>
2023-08-20 15:27:24 -0300

    previa-mapa-cadastro-farmacia

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import { LoginPage } from "./pages/login";
-// import { MedicamentosPage } from "./pages/medicamentos";
-// import { CadastroDeMedicamentosPage } from "./pages/cadastroDeMedicamentos"
-// import { CadastroFarmaciaPage } from "./pages/cadastroDeFarmacias";
+import { MedicamentosPage } from "./pages/medicamentos";
+import { CadastroDeMedicamentosPage } from "./pages/cadastroDeMedicamentos"
+import { CadastroFarmaciaPage } from "./pages/cadastroDeFarmacias";
 import { MapaDeFarmaciasPage } from "./pages/mapaDeFarmacias";
 import App from "./App";
 import { NavBar } from "./components/Header";
@@ -17,9 +17,9 @@ const router = createBrowserRouter([
     element: <App/>,
     children: [
     {path:"/", element:<LoginPage />}, {path:"/", element:<NavBar />},
-      // {path:"/cadastroDeMedicamentos", element:<CadastroDeMedicamentosPage />},
-      // {path:"/cadastroFarmacias", element:<CadastroFarmaciaPage />},
-      // {path:"/medicamentos", element:< MedicamentosPage/>},
+      {path:"/cadastroDeMedicamentos", element:<CadastroDeMedicamentosPage />},
+      {path:"/cadastroFarmacias", element:<CadastroFarmaciaPage />},
+      {path:"/medicamentos", element:< MedicamentosPage/>},
       {path:"/map", element:< MapaDeFarmaciasPage />}
     ]
   }
