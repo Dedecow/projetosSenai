6008a27900acc99dc62886b545c7d0041a87bf5c
Andreh Silveira <andrehbahia@gmail.com>
2023-08-26 16:22:59 -0300

    parcial-cadastro

--- src/components/CadastroDeFarmacia/index.jsx
+++ src/components/CadastroDeFarmacia/index.jsx
@@ -4,6 +4,47 @@ import './styled.css';
 import { PuxarCoordenadas } from "../Maps/PuxarCoordenadas";
 // import axios from "axios";
 
+
+function CoordinatesDisplay({ latitude, longitude }) {
+    return (
+        <div>
+            {/* <p>Latitude: {latitude}</p>
+            <p>Longitude: {longitude}</p> */}
+        </div>
+    );
+}
+
+function PuxarCoordenadas({ onUpdateCoords }) {
+    const [userLocation, setUserLocation] = useState({ lat: 0, lng: 0 });
+
+    useEffect(() => {
+        const getUserLocation = () => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    position => {
+                        const { latitude, longitude } = position.coords;
+                        setUserLocation({ lat: latitude, lng: longitude });
+                        onUpdateCoords(latitude, longitude);
+                    },
+                    error => {
+                        console.error(error);
+                    }
+                );
+            } else {
+                console.error('Não foi possível obter as coordenadas.');
+            }
+        };
+        getUserLocation();
+    }, [onUpdateCoords]);
+
+    return (
+        <CoordinatesDisplay
+            latitude={userLocation.lat}
+            longitude={userLocation.lng}
+        />
+    );
+}
+
 function CadastroDeFarmacia (){
 
     // const [ dados, setDados] = useState ([]);
