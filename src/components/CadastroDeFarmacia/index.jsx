46eb16fd404651e24fdc480a26306400c6e3cd3d
Andreh Silveira <andrehbahia@gmail.com>
2023-08-26 15:24:15 -0300

    pegou-coordenadas-manual

--- src/components/CadastroDeFarmacia/index.jsx
+++ src/components/CadastroDeFarmacia/index.jsx
@@ -37,6 +37,55 @@ function CadastroDeFarmacia (){
         localStorage.setItem('lista_farmacias', JSON.stringify(lista))
     }, [lista])
 
+    function CoordinatesDisplay({ latitude, longitude }) {
+        return (
+            <div>
+                {/* <p>Latitude: {latitude}</p>
+                <p>Longitude: {longitude}</p> */}
+            </div>
+        );
+    }
+    
+    function PuxarCoordenadas({ onUpdateCoords }) {
+        const [userLocation, setUserLocation] = useState({ lat: 0, lng: 0 });
+    
+        useEffect(() => {
+            const getUserLocation = () => {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(
+                        position => {
+                            const { latitude, longitude } = position.coords;
+                            setUserLocation({ lat: latitude, lng: longitude });
+                            onUpdateCoords(latitude, longitude);
+                        },
+                        error => {
+                            console.error(error);
+                        }
+                    );
+                } else {
+                    console.error('Não foi possível obter as coordenadas.');
+                }
+            };
+            getUserLocation();
+        }, [onUpdateCoords]);
+    
+        return (
+            <CoordinatesDisplay
+                latitude={userLocation.lat}
+                longitude={userLocation.lng}
+            />
+        );
+    }
+    const atualizarCoordenadas = (lat, lng) => {
+        setFarmacias({
+            ...farmacias,
+            coordenadas: {
+                ...farmacias.coordenadas,
+                latitude: lat,
+                longitude: lng
+            }
+        });
+    };
     const handleChange = (e) => {
         const {name, value} = e.target;
         setFarmacias({...farmacias, [name]: value});
@@ -92,9 +141,24 @@ function CadastroDeFarmacia (){
             <TextField label="Estado" required = {true} variant="outlined" name="estado" value={farmacias.estado} onChange={handleChange} />
             <TextField label="Complemento" required = {false} variant="outlined" name="complemento" value={farmacias.complemento} onChange={handleChange} />
             <div className="latitude-longitude-container">
-            <TextField label="Latitude" required variant="outlined" name="latitude" value={farmacias.latitude} onChange={handleChange} />
-            <TextField label="Longitude" required variant="outlined" name="longitude" value={farmacias.longitude} onChange={handleChange} />
-            <Button className="current-location-button" variant="contained" onClick={PuxarCoordenadas}>Puxar Coordenadas Atuais</Button>
+                    <TextField
+                        label="Latitude"
+                        required
+                        variant="outlined"
+                        name="latitude"
+                        value={farmacias.coordenadas?.latitude || ''}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Longitude"
+                        required
+                        variant="outlined"
+                        name="longitude"
+                        value={farmacias.coordenadas?.longitude || ''}
+                        onChange={handleChange}
+                    />
+                    <PuxarCoordenadas onUpdateCoords={atualizarCoordenadas} />
+            <Button className="current-location-button" variant="contained" onClick={PuxarCoordenadas}>Inserir Coordenadas Manualmente</Button>
             </div> 
 
             </div>
