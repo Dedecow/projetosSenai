1dfa01f9db1c1c46283e6585c05d0f395d4668d7
Andreh Silveira <andrehbahia@gmail.com>
2023-08-26 15:42:23 -0300

    tenta-alterar-cep

--- src/components/CadastroDeFarmacia/index.jsx
+++ src/components/CadastroDeFarmacia/index.jsx
@@ -97,7 +97,7 @@ function CadastroDeFarmacia (){
 
 
      
-     async function pegarCep () {
+    async function pegarCep () {
         await fetch (`http://viacep.com.br/ws/${farmacias.cep}/json/`)
         .then(resposta => resposta.json() )
         .then (dados => {
@@ -111,6 +111,7 @@ function CadastroDeFarmacia (){
             });
             console.log(dados)
         })   
+   
 
        
      }
@@ -122,23 +123,84 @@ function CadastroDeFarmacia (){
             <p> Cadastro de Farmácia </p>
 
             <div className="text-field">
-            <TextField label="Razão Social" required = {true} variant="outlined" name="razaoSocial" value={farmacias.razaoSocial} onChange={handleChange} />
-            <TextField label="CNPJ" required = {true} variant="outlined" name="cnpj" value={farmacias.cnpj} onChange={handleChange} />
-            <TextField label="Nome Fantasia" required = {true} variant="outlined" name="nomeFantasia" value={farmacias.nomeFantasia} onChange={handleChange} />
-            <TextField label="E-Mail" required = {true} variant="outlined" name="email" value={farmacias.email} onChange={handleChange} />
-            <TextField label="Telefone" required = {true} variant="outlined" name="telefone" value={farmacias.telefone} onChange={handleChange} />
-            <TextField label="Celular" required = {true} variant="outlined" name="celular" value={farmacias.celular} onChange={handleChange} />
+            <TextField 
+            label="Razão Social" 
+            required = {true} 
+            variant="outlined" 
+            name="razaoSocial" 
+            value={farmacias.razaoSocial} 
+            onChange={handleChange} />
+
+            <TextField 
+            label="CNPJ" 
+            required = {true} 
+            variant="outlined" 
+            name="cnpj" 
+            value={farmacias.cnpj} 
+            onChange={handleChange} />
+
+            <TextField 
+            label="Nome Fantasia" 
+            required = {true} 
+            variant="outlined" 
+            name="nomeFantasia" 
+            value={farmacias.nomeFantasia} 
+            onChange={handleChange} />
+
+            <TextField 
+            label="E-Mail" 
+            required = {true} 
+            variant="outlined" 
+            name="email" 
+            value={farmacias.email} 
+            onChange={handleChange} />
+
+            <TextField label="Telefone" 
+            required = {true} 
+            variant="outlined" 
+            name="telefone" 
+            value={farmacias.telefone} 
+            onChange={handleChange} />
+
+            <TextField 
+            label="Celular" 
+            required = {true} 
+            variant="outlined" 
+            name="celular" 
+            value={farmacias.celular} 
+            onChange={handleChange} />
+
             </div>
+
             <div className="text-field">
+
             <div className="buscar-cep-container">
-             <TextField className="buscar-cep-imput" label="CEP" required = {true} variant="outlined" name="cep" value={farmacias.cep} onChange={handleChange} /> 
-             <Button className="buscar-cep-button" variant="contained" onClick={pegarCep}>Buscar CEP</Button>  
+             <TextField 
+            className="buscar-cep-imput" 
+            label="CEP" 
+            required = {true} 
+            variant="outlined" 
+            name="cep" 
+            value={farmacias.cep} 
+            onChange={handleChange} /> 
+                <Button 
+            className="buscar-cep-button" 
+            variant="contained" 
+            onClick={pegarCep}>
+            Buscar CEP
+            </Button>  
             </div>
             <TextField label="Logradouro" required = {true} variant="outlined" name="logradouro" value={farmacias.logradouro} onChange={handleChange} />
+
             <TextField label="Numero" required = {true} variant="outlined" name="numero" value={farmacias.numero} onChange={handleChange} />
+
             <TextField label="Bairro" required = {true} variant="outlined" name="bairro" value={farmacias.bairro} onChange={handleChange} />
+
+
             <TextField label="Cidade" required = {true} variant="outlined" name="cidade" value={farmacias.cidade} onChange={handleChange} />
+            
             <TextField label="Estado" required = {true} variant="outlined" name="estado" value={farmacias.estado} onChange={handleChange} />
+
             <TextField label="Complemento" required = {false} variant="outlined" name="complemento" value={farmacias.complemento} onChange={handleChange} />
             <div className="latitude-longitude-container">
                     <TextField
