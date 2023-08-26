0f39fdd4c32079be3365261b3f28041bfeba99c1
Andreh Silveira <andrehbahia@gmail.com>
2023-08-26 10:01:17 -0300

    funcao-pegar-localizacao

--- src/components/Maps/PuxarCoordenadas.jsx
+++ src/components/Maps/PuxarCoordenadas.jsx
@@ -1,4 +1,25 @@
 function PuxarCoordenadas () {
+    const [userLocation, setUserLocation] = useState({ lat: 0, lng: 0 });
+
+    useEffect(() => {
+        const getUserLocation = () => {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+              position => {
+                const { latitude, longitude } = position.coords;
+                setUserLocation({ lat: latitude, lng: longitude });
+              },
+              error => {
+                console.error(error);
+              }
+            );
+          } else {
+            console.error('Tentativa de pegar as coordenadas.');
+          }
+        };
+        console.log("Coordenadas",getUserLocation);
+        getUserLocation();
+      }, []);
 
     return(
             <p> Puxando coordenadas </p>
