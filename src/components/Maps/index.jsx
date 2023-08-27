baa3d35a106bac3f447b3cc416e7fd15cac0dd51
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-08-27 20:11:07 -0300

    renderizar-mapa

--- src/components/Maps/index.jsx
+++ src/components/Maps/index.jsx
@@ -1,27 +1,43 @@
-import React, { useEffect } from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import React, { useState, useEffect } from 'react';
+import { LatLngExpression } from 'leaflet';
+
 
 function MapComponent() {
-  useEffect(() => {
-    // Create a new map instance
-    const map = L.map('map').setView([-27.585594, -48.460666], 15);
+  
 
-    // Add the tile layer to the map
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    // Add a marker to the map
-    L.marker([-27.585594, -48.460666]).addTo(map)
-      .bindPopup('Ilha de Florianópolis')
-      .openPopup();
-  }, []);
 
   return (
-    <div>
-      <div id="map" style={{ height: '500px', width: '100%' }}></div>
-    </div>
+    <container>
+      <MapContainer
+          center={
+            {
+              lat: coords[0],
+              lng: coords[1],
+            } as LatLngExpression
+          }
+          zoom={13}
+          whenCreated={(map) => {
+            map.addEventListener("click", (event: LeafletMouseEvent) => {
+              setFormValues((prev) => ({
+                ...prev,
+                coords: [event.latlng.lat, event.latlng.lng],
+              }));
+            });
+          }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker
+            position={
+              [formValues.coords[0], formValues.coords[1]] as LatLngExpression
+            }
+          />
+        </MapContainer>
+
+
+    </container>
   );
 }
 
